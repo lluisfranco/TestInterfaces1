411b43cc8757bcebbee95263377887b61801f2dc
Lluis Franco <hello@lluisfranco.com>
2025-06-05 11:12:19 +0200

    Add IAnimalWithOwner interface and update animal classes
    Introduced a new interface `IAnimalWithOwner` that extends `IAnimal` and `IOwnerInfo`. Updated `Dog` and `Cat` classes to implement this interface. Modified `Program.cs` to manage a list of `IAnimalWithOwner` objects and added an `AnimalService` class for feeding and calling owners. Enhanced `Eat` and `Sleep` methods with class-specific messages. Retained commented-out sections for potential future file parsing functionality.

--- TestInterfaces1/Program.cs
+++ TestInterfaces1/Program.cs
@@ -1,20 +1,77 @@
-﻿namespace TestInterfaces1
+﻿using System.Xml;
+
+namespace TestInterfaces1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var pets = new List<IAnimalWithOwner>(); //generics
             var mydog = new Dog
             {
                 Name = "Bobby",
                 OwnerName = "Ven",
                 LegsNumber = 3
             };
-            mydog.Eat();
-            mydog.Sleep();
+            var mycat = new Cat
+            {
+                Name = "Mia",
+                OwnerName = "Lluis",
+                LegsNumber = 4
+            };
+            pets.Add(mydog);
+            pets.Add(mycat);
+
+            foreach (var pet in pets)
+            {
+                AnimalService.FeedAnimalAndPutAnimalToSleep(pet);
+            }
             Console.ReadLine();
         }
     }
+
+    //public class ParserService
+    //{
+    //    public static void ParseFiles()
+    //    {
+    //        var savedFiles = GetSavedFilesFromDatabase();
+    //        IParser andBankParser = new AndbankParser();
+    //        IParser moabankParser = new MoabankParser();
+
+    //        foreach (var savedFile in savedFiles)
+    //        {
+    //            if(savedFile.type == "AND") andBankParser.Parse(savedFile.content);
+    //            if(savedFile.type == "MOA") moabankParser.Parse(savedFile.content);
+    //        }
+    //    }
+    //}
+
+
+
+    //public interface IParser
+    //{
+    //    public void Parse(string input);
+    //}
+
+    //public class AndbankParser : IParser
+    //{
+    //    public void Parse(string input)
+    //    {
+    //        // Implementation for Andbank parsing logic
+    //        //parse the input as a CSV file
+
+    //    }
+    //}
+
+    //public class MoabankParser : IParser
+    //{
+    //    public void Parse(string input)
+    //    {
+    //        // Implementation for Moabank parsing logic
+    //        //parse the input as a XML file
+
+    //    }
+    //}
 
     public interface IAnimal
     {
@@ -27,35 +84,62 @@
     public interface IOwnerInfo
     {
         public string OwnerName { get; set; }
+        public void CallOwner();
     }
 
-    public class Dog : IAnimal, IOwnerInfo
+    public interface IAnimalWithOwner : IAnimal, IOwnerInfo
+    {
+
+    }
+
+    public class Dog : IAnimalWithOwner
     {
         public string Name { get; set; } = "";
         public int LegsNumber { get; set; } = 4; //default
         public void Eat()
         {
-            Console.WriteLine($"{Name} is eating. Thx { OwnerName}");
+            Console.WriteLine($"DOG CLASS {Name} is eating. Thx { OwnerName}");
         }
         public void Sleep()
         {
-            Console.WriteLine($"{Name} is sleeping. Time to relax {OwnerName}");
+            Console.WriteLine($"DOG CLASS {Name} is sleeping. Time to relax {OwnerName}");
         }
         public string OwnerName { get; set; } = "";
+        public void CallOwner()
+        {
+            Console.WriteLine($"DOG CLASS Calling {OwnerName} for {Name}");
+        }
     }
 
-    public class Cat : IAnimal, IOwnerInfo
+    public class Cat : IAnimalWithOwner
     {
         public string Name { get; set; } = "";
         public int LegsNumber { get; set; } = 4; //default
         public void Eat()
         {
-            Console.WriteLine($"{Name} is eating a mice. Thx for nothing {OwnerName}");
+            Console.WriteLine($"CAT CLASS {Name} is eating a mice. Thx for nothing {OwnerName}");
         }
         public void Sleep()
         {
-            Console.WriteLine($"{Name} is not sleeping. Time to find it {OwnerName}");
+            Console.WriteLine($"CAT CLASS {Name} is not sleeping. Time to find it {OwnerName}");
         }
         public string OwnerName { get; set; } = "";
+        public void CallOwner()
+        {
+            Console.WriteLine($"CAT CLASS Calling owner {OwnerName}");
+        }
+    }
+
+    public class AnimalService
+    {
+        public static void FeedAnimalAndPutAnimalToSleep(IAnimalWithOwner animal)
+        {
+            Console.WriteLine($"SERVICE INIT");
+            animal.Eat();
+            animal.Sleep();
+            Console.WriteLine($"SERVICE Animal {animal.Name} is sleeping. Calling { animal.OwnerName}");
+            animal.CallOwner();
+            Console.WriteLine($"SERVICE END");
+        }
     }
 }
